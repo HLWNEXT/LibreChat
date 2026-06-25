2b5ed4836802be3ccc740d90d525bb7212c276a7
Chenzhang Wang <51490717+czwangxtt@users.noreply.github.com>
2026-06-25 11:24:00 -0400

    feat: move login logo inside card container

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -59,15 +59,6 @@ function AuthLayout({
   return (
     <div className="relative flex min-h-screen flex-col bg-white dark:bg-gray-900">
       <Banner />
-      <BlinkAnimation active={isFetching}>
-        <div className="mt-6 h-48 w-full bg-cover">
-          <img
-            src="assets/logo.svg"
-            className="h-full w-full object-contain dark:invert"
-            alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'LibreChat' })}
-          />
-        </div>
-      </BlinkAnimation>
       <DisplayError />
       <div className="absolute bottom-0 left-0 md:m-4">
         <ThemeSelector />
@@ -75,6 +66,15 @@ function AuthLayout({
 
       <main className="flex flex-grow items-center justify-center">
         <div className="w-authPageWidth overflow-hidden bg-white px-6 py-4 dark:bg-gray-900 sm:max-w-md sm:rounded-lg">
+          <BlinkAnimation active={isFetching}>
+            <div className="mb-4 h-48 w-full">
+              <img
+                src="assets/logo.svg"
+                className="h-full w-full object-contain dark:invert"
+                alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'LibreChat' })}
+              />
+            </div>
+          </BlinkAnimation>
           {!hasStartupConfigError && !isFetching && header && (
             <h1
               className="mb-4 text-center text-3xl font-semibold text-black dark:text-white"
